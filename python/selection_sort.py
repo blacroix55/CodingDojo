b3f514eac4db2b25c3e7f85d7125ed71074a649c
Brian LaCroix <blacroix@verizonmedia.com>
2019-07-08 16:22:18 -0500

    adding selection_sort.py

---
+++ python/selection_sort.py
@@ -0,0 +1,13 @@
+arr=[5,7,8,2,3,1,9,6,0,4]
+
+print ("start =",arr)
+
+for workingPosition in range(0,len(arr)):
+    lowestPosition=workingPosition
+    for comparisonValue in range(workingPosition,len(arr)):
+        if arr[comparisonValue]<arr[lowestPosition]:
+            lowestPosition=comparisonValue
+    if lowestPosition!=workingPosition:
+        arr[workingPosition],arr[lowestPosition]=arr[lowestPosition],arr[workingPosition]
+
+print ("end =",arr)
