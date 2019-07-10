8a3cb3cd8f48d363176f6be29b531bb7699ae328
Brian LaCroix <blacroix@verizonmedia.com>
2019-07-10 16:14:07 -0500

    adding flask projects

---
+++ python/flask_fundamentals/understanding_routing/server.py
@@ -0,0 +1,36 @@
+from flask import Flask  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
+
+@app.route('/dojo')          # The "@" decorator associates this route with the function immediately following
+def dojo():
+    return 'Dojo!'  # Return the string 'Hello World!' as a response
+
+@app.route('/say/<string:name>')
+def say(name):
+    print ("*"*40)
+    print ("executing say function")
+    if type(name) != str :
+        return f"{name} is not a string and string is required.  Try again!"
+    return f"Hello, {name}"
+
+@app.route('/<int:repeat>/<string:word>')
+def repeat_word(repeat,word):
+    print ("*"*40)
+    print ("executing repeat_word function")
+    ans=f""
+    for x in range(repeat):
+       ans += f"{word} "
+    return ans
+
+@app.route("/<path:u_path>")
+def you_suck(u_path):
+    print ("*"*40)
+    print ("executing you_such function")
+    return "You suck - and put in an invalid path!  Try again."
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
