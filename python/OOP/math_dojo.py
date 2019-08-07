c29d069a3e7311a8657218f78ac73336adc7bc20
Brian LaCroix <blacroix@verizonmedia.com>
2019-08-07 07:05:40 -0700

    finishing math_dojo assignment

---
+++ python/OOP/math_dojo.py
@@ -0,0 +1,27 @@
+class MathDojo:
+    def __init__(self):
+        self.result = 0
+    def add(self, num, *nums):
+        self.result += num
+        if nums:
+            for n in nums:
+                self.result += n
+        return self
+    def subtract(self, num, *nums):
+        self.result -= num
+        if nums:
+            for n in nums:
+                self.result -= n
+        return self
+
+# create an instance:
+md = MathDojo()
+
+# to test:
+x = md.add(2).add(2,5,1).subtract(3,2).result
+print(x)	# should print 5
+
+# run each of the methods a few more times and check the result!
+brian = MathDojo() # runs __init__, which in this case sets brian.result to 0
+y = brian.add(5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1).subtract(1,1,1,1,1,1,1,1,1,1).result
+print (y)
