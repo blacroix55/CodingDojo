a196556399717ef5e8890f9b7eea27a0625f97bc
Brian LaCroix <blacroix@verizonmedia.com>
2019-08-08 13:52:11 -0700

    finishing C+R Users project

--- python/OOP/first_orm_project/app.py
+++ python/OOP/first_orm_project/app.py
@@ -19,6 +19,7 @@ class User(db.Model):
     first_name = db.Column(db.String(45))
     last_name = db.Column(db.String(45))
     email = db.Column(db.String(45))
+    age = db.Column(db.String(4))
     created_at = db.Column(db.DateTime, server_default=func.now())    # notice the extra import statement above
     updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
 
@@ -27,7 +28,28 @@ class User(db.Model):
 print ('at routes')
 @app.route('/')
 def index():
-    return render_template("index.html")
+    users = User.query.all()
+    print ('*'*80)
+    print ('all user data:')
+    for user in users:
+        print (user.first_name,user.last_name,user.email)
+    return render_template("index.html", users=users)
+
+@app.route("/users/create", methods=["POST"])
+def user_create():
+    print('*'*80)
+    print ('Received add_new_user form with the following data:')
+    print ('FORM DATA RECEIVED:\n',request.form)
+    new_user = User(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        email=request.form['email'],
+        age=request.form['age']
+        )
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
