bacec957f8a38b942db5ac74a2465eba28c01520
Brian LaCroix <blacroix@verizonmedia.com>
2019-08-13 14:05:43 -0500

    starting to modularize

---
+++ python/modularization/dojos_and_ninjas/config.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy			# instead of mysqlconnection
+from flask_migrate import Migrate			# this is new
+app = Flask(__name__)
+# configurations to tell our app about the database we'll be connecting to
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users_and_dojos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# an instance of the ORM
+db = SQLAlchemy(app)
+# a tool for allowing migrations/creation of tables
+migrate = Migrate(app, db)
